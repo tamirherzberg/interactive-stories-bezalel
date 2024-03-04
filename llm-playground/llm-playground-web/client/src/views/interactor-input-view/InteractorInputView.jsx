e10f6a26ae8b3fd9d7fdcb0456a64c12963cb918
tamirherzberg <tamir.herzberg@gmail.com>
2024-03-04 13:13:08 +0200

    Center ending

--- llm-playground/llm-playground-web/client/src/views/interactor-input-view/InteractorInputView.jsx
+++ llm-playground/llm-playground-web/client/src/views/interactor-input-view/InteractorInputView.jsx
@@ -54,18 +54,21 @@ export default function InteractorInputView() {
                 opacity: status === 'loading' ? 0.3 : 1,
                 pointerEvents: status === 'loading' ? 'none' : 'auto',
                 color: status === 'error' ? 'red' : 'auto',
-                fontFamily: 'Arial'
+                fontFamily: 'Arial',
+                display: 'flex',
+                flexDirection: (curStrangerIdx == 3 || curStrangerIdx == 4) ? 'column' : 'unset',
+                alignItems: 'center',
             }}
         >
-            <img src="https://media1.tenor.com/m/07cmziHz9hQAAAAC/curb-your-enthusiasm-curb-your-enthusiasm-outro.gif" alt="FIN" style={{display: curStrangerIdx == 4 ? 'inherit':'none'}}/>
-            <div style={{display: curStrangerIdx == 3 ? 'none': 'inherit'}}>Tsila:&ensp;</div><input
+            <img src="https://c.tenor.com/07cmziHz9hQAAAAC/tenor.gif" alt="FIN" style={{justifyContent: 'center', display: curStrangerIdx == 4 ? 'inherit':'none'}}/>
+            <div style={{display: (curStrangerIdx == 3 || curStrangerIdx == 4) ? 'none': 'inherit'}}>Tsila:&ensp;</div><input
                 id="interactor-text-input"
                 value={inputMessage}
                 onKeyDown={e => { if (e.key === 'Enter') send() }}
                 onChange={e => setAppState({ inputMessage: e.target.value })}
-                style={{display: curStrangerIdx == 3 ? 'none': 'inherit'}}
+                style={{display: (curStrangerIdx == 3 || curStrangerIdx == 4) ? 'none': 'inherit'}}
             />
-            <button onClick={send} style={{display: curStrangerIdx == 3 ? 'none': 'inherit'}}>Send</button>
+            <button onClick={send} style={{display: (curStrangerIdx == 3 || curStrangerIdx == 4) ? 'none': 'inherit'}}>Send</button>
             {
                 status === 'error' && 'Something is broken 😵‍💫'
             }
